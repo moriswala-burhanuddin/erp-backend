7e5b42ad909c637878b0ef2aa7d45bb0b2ebf9ac
burhanaare <firegamingbfire@gmail.com>
2026-02-12 10:59:37 +0530

    Backend for VPS deployment

--- storeflow_backend/settings.py
+++ storeflow_backend/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
